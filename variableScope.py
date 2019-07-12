05456b4e6839074636255a550bf21149ac16cfa6
KorraHaswi <salmanzaidy@gmail.com>
2019-07-12 11:34:21 -0700

    verifying the scope of variables

---
+++ variableScope.py
@@ -0,0 +1,65 @@
+=============================================SCENARIO 1===============================================================
+x=1
+def foo():
+    #x='salman'
+    print(x)
+    def koo():
+        #x=3
+        print(x)
+    koo()
+    return x
+
+print(x)
+print(foo())
+print(x)
+
+output
+1
+1
+1
+1
+1
+==========================================SCENARIO 2=======================================================================
+x=1
+def foo():
+    #x='salman'
+    print(x)
+    def koo():
+        x=3
+        print(x)
+    koo()
+    return x
+
+print(x)
+print(foo())
+print(x)
+
+
+output
+1
+1
+3
+1
+1
+============================================SCENARIO 3=====================================================================
+
+
+x=1
+def foo():
+    x='salman'
+    print(x)
+    def koo():
+        x=3
+        print(x)
+    koo()
+    return x
+
+print(x)
+print(foo())
+print(x)
+
+1
+salman
+3
+salman
+1
